2ebcb5b513a0ecce39c24f5f4adde6f5e99085af
badwaikparas <111993734+badwaikparas@users.noreply.github.com>
2024-02-21 11:03:07 +0530

    screen 5 html done

---
+++ src/Screen/Screen5.js
@@ -0,0 +1,31 @@
+function Screen5() {
+    return (
+        <div class=" default-ltr-cache-v2xv94 e9eyrqp6" data-uia="nmhp-card-animation+kidsValueProp">
+            <div class="default-ltr-cache-pv2edp e9eyrqp1">
+                <div class="default-ltr-cache-ogx8b3 e9eyrqp5">
+                    <h2 data-uia="nmhp-card-animation-text-title" class="default-ltr-cache-jpuyb8 e9eyrqp8">Create profiles for
+                        kids</h2>
+                    <p data-uia="nmhp-card-animation-text-subtitle" class="default-ltr-cache-10i4ild e9eyrqp7">Send children on
+                        adventures with their favourite characters in a space made just for them—free with your membership.</p>
+                    <div class="default-ltr-cache-1asgq30 e9eyrqp4">
+                        <div class="center-pixel default-ltr-cache-1qms9jn ekwtkbw0"></div>
+                    </div>
+                </div>
+                <div class="default-ltr-cache-vgp0nn e9eyrqp3">
+                    <div class="default-ltr-cache-bjn8wh"><img alt=""
+                        src="https://occ-0-2186-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d"
+                        data-uia="nmhp-card-animation-asset-image" class="default-ltr-cache-1d3w5wq"></img>
+                        <div data-uia="nmhp-card-animation-asset-motion" class="default-ltr-cache-3snzoc e15c37ii15">
+                            <div class="default-ltr-cache-0"></div>
+                            <div data-uia="nmhp-card-animation-asset-custom" aria-hidden="true"
+                                class="default-ltr-cache-0 e15c37ii2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-uia="nmhp-card-animation+kidsValueProp+divider" class="default-ltr-cache-1yz1b0z e9eyrqp0"></div>
+        </div>
+    );
+}
+
+export default Screen5;
